33ca0cdabaf1d5af5bdef105b9e7942fb1277806
Philip Greyson <pgreyson@d118.org>
2024-02-21 10:17:31 -0600

    cleanup formatting, standardize to my other scripts

--- ethnicity.py
+++ ethnicity.py
@@ -6,97 +6,90 @@
 # https://groups.io/g/psug-il/message/1483
 
 # importing module
-import oracledb  # needed for connection to PowerSchool (oracle database)
+import datetime  # used to get current date for course info
 import os  # needed to get environment variables
+from datetime import *
+
+import oracledb  # needed for connection to PowerSchool (oracle database)
 import pysftp  # needed for sftp file upload
 
-un = 'PSNavigator'  # PSNavigator is read only, PS is read/write
-# the password for the PSNavigator account
-pw = os.environ.get('POWERSCHOOL_DB_PASSWORD')
-# the IP address, port, and database name to connect to
-cs = os.environ.get('POWERSCHOOL_PROD_DB')
+DB_UN = os.environ.get('POWERSCHOOL_READ_USER')  # username for read-only database user
+DB_PW = os.environ.get('POWERSCHOOL_DB_PASSWORD')  # the password for the database account
+DB_CS = os.environ.get('POWERSCHOOL_PROD_DB')  # the IP address, port, and database name to connect to
 
 #set up sftp login info
-sftpUN = os.environ.get('D118_SFTP_USERNAME')
-sftpPW = os.environ.get('D118_SFTP_PASSWORD')
-sftpHOST = os.environ.get('D118_SFTP_ADDRESS')
-# connection options to use the known_hosts file for key validation
-cnopts = pysftp.CnOpts(knownhosts='known_hosts')
+SFTP_UN = os.environ.get('D118_SFTP_USERNAME')  # username for the d118 sftp server
+SFTP_PW = os.environ.get('D118_SFTP_PASSWORD')  # password for the d118 sftp server
+SFTP_HOST = os.environ.get('D118_SFTP_ADDRESS')  # ip address/URL for the d118 sftp server
+CNOPTS = pysftp.CnOpts(knownhosts='known_hosts')  # connection options to use the known_hosts file for key validation
 
-print("Username: " + str(un) + " |Password: " + str(pw) + " |Server: " + str(cs)) #debug so we can see where oracle is trying to connect to/with
-print("SFTP Username: " + str(sftpUN) + " |SFTP Password: " + str(sftpPW) + " |SFTP Server: " + str(sftpHOST)) #debug so we can see what credentials are being used
+OUTPUT_FILE_NAME = 'ethnicity.txt'
+OUTPUT_FILE_DIRECTORY = '/sftp/ethnicity/'
 
-with open ('ethnicityLog.txt', 'w') as log:
-    # create the connecton to the database
-    with oracledb.connect(user=un, password=pw, dsn=cs) as con:
-        with con.cursor() as cur:  # start an entry cursor
-            with open('ethnicity.txt', 'w') as output:  # open the output file
-                print("Connection established: " + con.version)
-                #SQL Query credit to Richard Moeller https://groups.io/g/psug-il/message/1483
-                cur.execute("WITH race AS \
-                (SELECT STUDENTID, CASE WHEN COUNT(DISTINCT RACECD)>1 THEN '17' ELSE MAX(RACECD) END AS CODE FROM STUDENTRACE GROUP BY STUDENTID) \
-                SELECT \
-                    stu.STUDENT_NUMBER, ildemo.FER, DECODE(stu.FEDETHNICITY,1,11,race.CODE) \
-                FROM \
-                    students stu \
-                    INNER JOIN race \
-                        ON stu.ID = race.STUDENTID \
-                    LEFT OUTER JOIN S_IL_STU_Demographics_X ildemo \
-                        ON stu.DCID = ildemo.STUDENTSDCID \
-                WHERE \
-                    stu.ENROLL_STATUS IN (0,-1)")
-                    # AND DECODE(stu.FEDETHNICITY,1,11,race.CODE) <> COALESCE(ildemo.FER,'00')")  # this makes it so it only includes them if they have a different value than what it should be.
-                rows = cur.fetchall() #fetchall() is used to fetch all records from result set and store the data from the query into the rows variable
+print(f"Database Username: {DB_UN} |Password: {DB_PW} |Server: {DB_CS}")  # debug so we can see where oracle is trying to connect to/with
+print(f'SFTP Username: {SFTP_UN} | SFTP Password: {SFTP_PW} | SFTP Server: {SFTP_HOST}')  # debug so we can see what info sftp connection is using
 
-                # RECREATION OF SCRIPT BELOW BUT BROKEN OUT FOR BETTER TROUBLESHOOTING
-                # cur.execute("SELECT STUDENTID, CASE WHEN COUNT(DISTINCT RACECD)>1 THEN '17' ELSE MAX(RACECD) END AS CODE FROM STUDENTRACE GROUP BY STUDENTID") # get each entry in the studentrace table, grouped by studentid. If they have more than one entry, they get code 17 otherwise they get the racecd code
-                # studentRaces = cur.fetchall()
-                # # print(studentRaces)
-                # for student in studentRaces:
-                #     try:
-                #         stuID = int(student[0]) if student[0] else ''
-                #         stuRaceCode = int(student[1]) if student[1] else ''# get the race code that will be used as their new FER code
-                #         # take the studentID from above and find the matching students student_number, their federal ethnicity flag, and their current FER code
-                #         cur.execute(f"SELECT students.student_number, students.fedethnicity, s_il_stu_demographics_x.fer, students.enroll_status FROM students INNER JOIN s_il_stu_demographics_x ON students.dcid = s_il_stu_demographics_x.studentsdcid WHERE students.id = {stuID}")
-                #         rows = cur.fetchall()
-                #         if rows:
-                #             # print(rows)
-                #             stuNumber = int(rows[0][0]) if rows[0][0] else ''# get the actual student number/id
-                #             ethnicityFlag = int(rows[0][1]) if rows[0][1] else ''# get their federal ethnicity flag
-                #             fer = int(rows[0][2]) if rows[0][2] else ''# get the current FER code
-                #             enroll = int(rows[0][3]) if rows[0][1] else '' # get their enrollment status
-                #             # print(f'StuNum: {stuNumber} | FER: {fer} | Race Code: {stuRaceCode} | Ethnicity: {ethnicityFlag} | StuID: {stuID}')
-                #             if ethnicityFlag == 1:
-                #                 ethnicityCode = 11 # if they are marked as hispanic from the ethnicity flag they get code 11
-                #             else:
-                #                 ethnicityCode = stuRaceCode
-                #             # print(f'Flag: {ethnicityFlag} - {type(ethnicityFlag)} - Code: {ethnicityCode} - {type(ethnicityCode)}')
-                #             if (enroll == -1 or enroll == 0) and (fer != ethnicityCode): # if they are pre-registerd or currently enrolled and the new code does not match their current FER entry
-                #                 print(f'ACTION: Changing {stuNumber} from code {fer} to {ethnicityCode}')
-                #         else:
-                #             print(f'WARNING: No student entry found for ID {stuID}')
-                #     except Exception as er:
-                #         print(f'ERROR on student id {student[0]}: {er}')
-                
-                # print(rows)
-                for student in rows:
-                    try:
-                        stuID = int(student[0])
-                        ethnicityCode = int(student[2])
-                        print(f'{stuID} \t {ethnicityCode}', file=output)
-                        print(f'RESULT- ID:{stuID} | Current FER: {student[1]} | New Code: {ethnicityCode}', file=log)
-                    except Exception as er:
-                        print(f'ERROR on {student[0]} - {er}')
-                        print(f'ERROR on {student[0]} - {er}', file=log)
 
-    with pysftp.Connection(sftpHOST, username=sftpUN, password=sftpPW, cnopts=cnopts) as sftp:
-        print('SFTP connection established')
-        # print(sftp.pwd)  # debug to show current directory
-        # print(sftp.listdir())  # debug to show files and directories in our location
-        sftp.chdir('/sftp/ethnicity/')
-        # print(sftp.pwd) # debug to show current directory
-        # print(sftp.listdir())  # debug to show files and directories in our location
-        # upload the file onto the sftp server
-        sftp.put('ethnicity.txt')
-        print("ACTION: Ethnicity code file placed on D118 SFTP server")
-        print("ACTION: Ethnicity code file placed on D118 SFTP server", file=log)
+if __name__ == '__main__':  # main file execution
+    with open ('ethnicityLog.txt', 'w') as log:
+        startTime = datetime.now()
+        startTime = startTime.strftime('%H:%M:%S')
+        print(f'INFO: Execution started at {startTime}')
+        print(f'INFO: Execution started at {startTime}', file=log)
+        with open('ethnicity.txt', 'w') as output:  # open the output file
+            # create the connecton to the database
+            try:
+                with oracledb.connect(user=DB_UN, password=DB_PW, dsn=DB_CS) as con:
+                    with con.cursor() as cur:  # start an entry cursor
+                        print(f'INFO: Connection established to PS database on version: {con.version}')
+                        print(f'INFO: Connection established to PS database on version: {con.version}', file=log)
+                        #SQL Query credit to Richard Moeller https://groups.io/g/psug-il/message/1483
+                        cur.execute("WITH race AS \
+                        (SELECT STUDENTID, CASE WHEN COUNT(DISTINCT RACECD)>1 THEN '17' ELSE MAX(RACECD) END AS CODE FROM STUDENTRACE GROUP BY STUDENTID) \
+                        SELECT \
+                            stu.STUDENT_NUMBER, ildemo.FER, DECODE(stu.FEDETHNICITY,1,11,race.CODE) \
+                        FROM \
+                            students stu \
+                            INNER JOIN race \
+                                ON stu.ID = race.STUDENTID \
+                            LEFT OUTER JOIN S_IL_STU_Demographics_X ildemo \
+                                ON stu.DCID = ildemo.STUDENTSDCID \
+                        WHERE \
+                            stu.ENROLL_STATUS IN (0,-1)")
+                            # AND DECODE(stu.FEDETHNICITY,1,11,race.CODE) <> COALESCE(ildemo.FER,'00')")  # this makes it so it only includes them if they have a different value than what it should be.
+                        rows = cur.fetchall()  # fetchall() is used to fetch all records from result set and store the data from the query into the rows variable
+
+                        # print(rows)
+                        for student in rows:
+                            try:
+                                stuID = int(student[0])
+                                ethnicityCode = int(student[2])
+                                print(f'{stuID}\t{ethnicityCode}', file=output)
+                                print(f'RESULT- ID:{stuID} | Current FER: {student[1]} | New Code: {ethnicityCode}', file=log)
+                            except Exception as er:
+                                print(f'ERROR while processing student {student[0]}: {er}')
+                                print(f'ERROR while processing student {student[0]}: {er}', file=log)
+            except Exception as er:
+                print(f'ERROR while connecting to PowerSchool or doing query: {er}')
+                print(f'ERROR while connecting to PowerSchool or doing query: {er}', file=log)
+        try:
+            with pysftp.Connection(SFTP_HOST, username=SFTP_UN, password=SFTP_PW, cnopts=CNOPTS) as sftp:
+                print(f'INFO: SFTP connection to D118 at {SFTP_HOST} successfully established')
+                print(f'INFO: SFTP connection to D118 at {SFTP_HOST} successfully established', file=log)
+                # print(sftp.pwd)  # debug to show current directory
+                # print(sftp.listdir())  # debug to show files and directories in our location
+                sftp.chdir(OUTPUT_FILE_DIRECTORY)
+                # print(sftp.pwd) # debug to show current directory
+                # print(sftp.listdir())  # debug to show files and directories in our location
+                # upload the file onto the sftp server
+                sftp.put(OUTPUT_FILE_NAME)
+                print("INFO: Ethnicity code file placed on D118 SFTP server")
+                print("INFO: Ethnicity code file placed on D118 SFTP server", file=log)
+        except Exception as er:
+                print(f'ERROR while connecting to D118 SFTP server: {er}')
+                print(f'ERROR while connecting to D118 SFTP server: {er}', file=log)
+
+        endTime = datetime.now()
+        endTime = endTime.strftime('%H:%M:%S')
+        print(f'INFO: Execution ended at {endTime}')
+        print(f'INFO: Execution ended at {endTime}', file=log)
